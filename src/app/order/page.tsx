b68e41fc1fb37754aff3e688307efcc52f263d1f
khanhnd <khanh.nguyendanh.kiras98@gmail.com>
2024-03-29 08:38:41 +0700

    stash

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -5,12 +5,21 @@ import Navbar from "@/components/Navbar";
 import { useState } from "react";
 
 export default function Home() {
-const [count, setCount] = useState(1)
+  const [count, setCount] = useState(1)
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-base">
-      <Navbar />
+    <main className="min-h-screen max-w-screen-xl bg-gradient-to-t from-pink-300 to bg-pink-200 flex flex-col justify-between">
+      <header className="min-w-full flex justify-center items-center py-3">
+        <img src="./Logo.svg" />
+      </header>
 
+      <div className="ae-drop-container">
+
+      </div>
+
+      <footer className="ae-order-footer">
+        <span>꒰ა 𝑳𝒆𝒕 𝒖𝒔 𝒆𝒎𝒃𝒓𝒂𝒄𝒆 𝒚𝒐𝒖𝒓 𝒈𝒊𝒓𝒍𝒚 𝒈𝒊𝒓𝒍 ໒꒱</span>
+      </footer>
     </main>
   );
 }
